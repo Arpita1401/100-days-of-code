786c4a9103405df757df8c0548b2155541759fba
Arpita Mohanty <58506260+Arpita1401@users.noreply.github.com>
2022-05-16 12:50:23 +0530

    Create Pascals triangle.cpp

---
+++ DAY-2/Pascals triangle.cpp
@@ -0,0 +1,23 @@
+
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> v;
+        // if(numRows==0)
+        //     return v;
+        vector<int> v1 ;
+        v1.push_back(1);
+        v.push_back(v1);
+        
+        for(int i=2;i<=numRows;i++){
+            vector<int>v2;
+            v2.push_back(1);
+            for(int j=1;j< i-1;j++)
+                v2.push_back( v[i-2][j]+ v[i-2][j-1]);
+            v2.push_back(1);
+            v.push_back(v2);
+        }
+        return v;
+    }
+};
