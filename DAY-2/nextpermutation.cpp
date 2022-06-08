68d5e812f1d215c42ba48aff9d30b91a03ff4dcf
Arpita Mohanty <58506260+Arpita1401@users.noreply.github.com>
2022-06-08 19:00:59 +0530

    Update nextpermutation.cpp

--- DAY-2/nextpermutation.cpp
+++ DAY-2/nextpermutation.cpp
@@ -1,12 +1,42 @@
 
-class Solution {
-public:
-    void nextPermutation(vector<int>& nums) {
+// class Solution {
+// public:
+//     void nextPermutation(vector<int>& nums) {
         
-     next_permutation(nums.begin(),nums.end());
-    }
-};
+//      next_permutation(nums.begin(),nums.end());
+//     }
+// };
 
 //time complexity: O(n!xn)
 
 
+
+#include <bits/stdc++.h> 
+vector<int> nextPermutation(vector<int> &p, int n)
+{
+    //  Write your code here.
+    int ind1=-1,ind2=-1;
+    for(int i=n-2;i>=0; i--)
+    {
+        if(p[i] < p[i+1]){
+            ind1= i;
+            break;
+        }
+    }
+    if(ind1==-1)
+    { reverse(p.begin(), p.end());
+     return p;
+    }
+    for(int i=n-1;i>ind1; i--)
+    {
+        if(p[i] > p[ind1]){
+            ind2= i;
+            break;
+        }
+    }
+    swap(p[ind1], p[ind2]);
+    reverse(p.begin()+ind1+1, p.end());
+    return p;
+        
+}
+//time complexity- 3xO(n)  , O(n)
